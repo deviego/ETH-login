37b9df8961d2a0bdf323fa9fc6e4a25905435d82
Deviego <deviego4@gmail.com>
2022-12-28 23:48:44 -0300

    add function redirect

--- script.js
+++ script.js
@@ -2,17 +2,23 @@ let span = document.querySelector('span')
 let button = document.querySelector('button')
 
 
+const redirectTime = () => {
+  window.location = "https://metamask.io/"
+}
 
-const HandleLogin = async ()=> {
+const HandleLogin = async () => {
   if(window.ethereum){
     try{
       const accounts = await ethereum.request({method:'eth_requestAccounts'})
       let account = accounts[0];
       span.innerHTML=`<h3> você esta logado com a conta: ${account}</h3>`
-    }catch(e){}
+    }catch(e){
+      alert('Tivemos complicações de um f5')
+    }
 
 }else{
   alert("não possui conta mask")
+  HandleLogin()
 }
 }
 button.addEventListener('click', HandleLogin())
